7cbfa69c3084ee034974a54ed46c14b67d0aa23b
Aravindhan TE <141827872+AravindhanAWS@users.noreply.github.com>
2026-04-11 16:24:55 +0530

    Refactor containsPattern method in Solution class

--- DetectPatternLength.py
+++ DetectPatternLength.py
@@ -1,13 +1,27 @@
-
-def containsPattern(self, arr: List[int], m: int, k: int) -> bool:
+class Solution:
+    def containsPattern(self, arr: List[int], m: int, k: int) -> bool:
         length = len(arr)
         count = 0
 
         for i in range(length - m):
-            if arr[i] == arr[i + 1]:
+            if arr[i] == arr[i + m]:
                 count += 1
 
                 if count ==  m * (k - 1):
                     return True
-            else: count == 0
+            else: 
+                count = 0
         return False
+
+                
+
+        
+
+
+
+
+        
+
+
+
+        
